91294d70b4a788485db2870c18a5ba79704086ed
Harmandeep Singh <76901738+Harman717@users.noreply.github.com>
2023-08-27 22:27:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-UPLOAD_FOLDER = os.path.join(os.path.expanduser('~'), 'Downloads', '20103063_rapidfort_project')
+UPLOAD_FOLDER = '/app/uploads'
 
 
 app = Flask(__name__)
