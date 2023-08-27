8d5b41fdaeef5f744e47d37355bf1f9ca669a857
Harmandeep Singh <76901738+Harman717@users.noreply.github.com>
2023-08-27 15:04:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import urllib.request
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+UPLOAD_FOLDER = 'C:\\Users\\simar\\Downloads\\20103063_rapidfort_project'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            flash('File successfully uploaded')
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
+
+    # Construct file_info dictionary even if no file is uploaded
+    file_info = {
+        'filename': filename if file else None,
+        'file_size': os.path.getsize(file_path) if file else None,
+        'file_url': urllib.request.pathname2url(file_path) if file else None
+    }
+
+    return render_template('upload.html', file_info=file_info)
+
+if __name__ == "__main__":
+    app.run()
